d65b4e77642ea0e97607713b9004195dee6c8cc8
Dustin Grange <dustin.grange@gmail.com>
2016-08-18 14:49:04 -0600

    Fixed an ambiguous splat operator

--- app/helpers/spree/wishlists/api_helpers.rb
+++ app/helpers/spree/wishlists/api_helpers.rb
@@ -6,7 +6,7 @@ module Spree
         :wished_product_attributes,
       ]
 
-      mattr_reader *ATTRIBUTES
+      mattr_reader(*ATTRIBUTES)
 
       @@wishlist_attributes = [
         :access_hash, :user_id, :name, :is_private, :is_default
