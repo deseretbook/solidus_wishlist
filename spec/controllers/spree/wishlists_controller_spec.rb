bfa61fa5c27fb18f9b1c359b23fbf572f9912ead
Taylor Scott <tscott@deseretbook.com>
2016-10-10 17:27:51 -0600

    fixed deprecations

--- spec/controllers/spree/wishlists_controller_spec.rb
+++ spec/controllers/spree/wishlists_controller_spec.rb
@@ -7,39 +7,39 @@ RSpec.describe Spree::WishlistsController, type: :controller do
 
   context '#new' do
     it 'assigns a new wishlist as @wishlist' do
-      spree_get :new
+      get :new
       expect(assigns(:wishlist)).to be_a_new Spree::Wishlist
     end
   end
 
   context '#index' do
     it 'assigns all wishlists as @wishlists' do
-      spree_get :index
+      get :index
       expect(assigns(:wishlists)).to eq user.wishlists
     end
   end
 
   context '#edit' do
     it 'assigns the requested wishlist as @wishlist' do
-      spree_get :edit, id: wishlist
+      get :edit, id: wishlist
       expect(assigns(:wishlist)).to eq wishlist
     end
   end
 
   context '#update' do
     it 'assigns @wishlist' do
-      spree_put :update, id: wishlist, wishlist: attributes
+      put :update, id: wishlist, wishlist: attributes
       expect(assigns(:wishlist)).to eq wishlist
     end
 
     context 'when the wishlist updates successfully' do
       it 'redirects to the updated wishlist' do
-        spree_put :update, id: wishlist, wishlist: attributes
+        put :update, id: wishlist, wishlist: attributes
         expect(response).to redirect_to wishlist
       end
 
       it 'sets the attributes of @wishlist according to attributes' do
-        spree_put :update, id: wishlist, wishlist: attributes
+        put :update, id: wishlist, wishlist: attributes
         attributes.each do |attr_name, value|
           expect(assigns(:wishlist).send(attr_name)).to eq value
         end
@@ -49,7 +49,7 @@ RSpec.describe Spree::WishlistsController, type: :controller do
     context 'when the wishlist fails to update' do
       it 'raise error' do
         expect {
-          spree_put :update, id: wishlist, wishlist: {}
+          put :update, id: wishlist, wishlist: {}
         }.to raise_error
       end
     end
@@ -57,14 +57,14 @@ RSpec.describe Spree::WishlistsController, type: :controller do
 
   context '#show' do
     it 'assigns the requested wishlist as @wishlist' do
-      spree_get :show, id: wishlist
+      get :show, id: wishlist
       expect(assigns(:wishlist)).to eq wishlist
     end
 
     # Regression test for issue #68
     it 'raises record not found on invalid params' do
       expect {
-        spree_get :show, id: 'nope'
+        get :show, id: 'nope'
       }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
@@ -72,45 +72,45 @@ RSpec.describe Spree::WishlistsController, type: :controller do
   context '#default' do
     it 'retrieves the default wishlist of the current user' do
       allow_any_instance_of(Spree::User).to receive(:wishlist)
-      spree_get :default
+      get :default
     end
 
     it 'assigns the default wishlist as @wishlist' do
-      spree_get :default
+      get :default
       expect(assigns(:wishlist)).to eq user.wishlist
     end
 
     it 'renders the wishlists/show template' do
-      spree_get :default
+      get :default
       expect(response).to render_template 'spree/wishlists/show'
     end
   end
 
   context '#create' do
     it 'assigns @wishlist' do
-      spree_post :create, wishlist: attributes
+      post :create, wishlist: attributes
       expect(assigns(:wishlist)).to be_a Spree::Wishlist
     end
 
     it 'sets the current user as the user of @wishlist' do
-      spree_post :create, wishlist: attributes
+      post :create, wishlist: attributes
       expect(assigns(:wishlist).user).to eq user
     end
 
     context 'when the wishlist saves successfully' do
       it 'saves the new wishlist' do
         expect {
-          spree_post :create, wishlist: attributes
+          post :create, wishlist: attributes
         }.to change(Spree::Wishlist, :count).by(1)
       end
 
       it 'redirects to the newly created wishlist' do
-        spree_post :create, wishlist: attributes
+        post :create, wishlist: attributes
         expect(response).to redirect_to user.wishlists.last
       end
 
       it 'sets the attributes of @wishlist according to attributes' do
-        spree_post :create, wishlist: attributes
+        post :create, wishlist: attributes
         attributes.each do |attr_name, value|
           expect(assigns(:wishlist).send(attr_name)).to eq value
         end
@@ -120,7 +120,7 @@ RSpec.describe Spree::WishlistsController, type: :controller do
     context 'when the wishlist fails to save' do
       it 'raise error' do
         expect {
-          spree_post :create, wishlist: {}
+          post :create, wishlist: {}
         }.to raise_error
       end
     end
@@ -129,12 +129,12 @@ RSpec.describe Spree::WishlistsController, type: :controller do
   context '#destroy' do
     it 'destroys the requested wishlist' do
       expect {
-        spree_delete :destroy, id: wishlist
+        delete :destroy, id: wishlist
       }.to change(Spree::Wishlist, :count).by(-1)
     end
 
     it 'redirects to the users account page' do
-      spree_delete :destroy, id: wishlist
+      delete :destroy, id: wishlist
       expect(response).to redirect_to spree.account_path
     end
   end
